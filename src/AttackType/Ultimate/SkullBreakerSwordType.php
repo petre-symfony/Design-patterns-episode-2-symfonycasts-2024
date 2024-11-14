22801ba2709cf93e2caab699b6edc8d9d411bbca
petrero <petrero@gmail.com>
2024-11-14 11:07:25 +0200

    15.2 But... wait. That class doesn't exist yet, right? Nope! But to save us some time, those new AttackType classes are already prepared in the tutorial/ directory at the root of our project. Open that up, copy the Ultimate/ folder, and paste it inside src/AttackType/.

---
+++ src/AttackType/Ultimate/SkullBreakerSwordType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\AttackType\Ultimate;
+
+use App\AttackType\AttackType;
+use App\Dice;
+
+class SkullBreakerSwordType implements AttackType {
+	public function performAttack(int $baseDamage): int {
+		$swordDamage = Dice::roll(20) + Dice::roll(20) + Dice::roll(20);
+
+		return ($baseDamage * 1.25) + $swordDamage;
+	}
+}
---
+++ src/AttackType/Ultimate/TitaniumBowType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\AttackType\Ultimate;
+
+use App\AttackType\AttackType;
+
+class TitaniumBowType implements AttackType {
+	/**
+	 * Always perform a critical strike.
+	 */
+	public function performAttack(int $baseDamage): int {
+		return $baseDamage * 3;
+	}
+}
