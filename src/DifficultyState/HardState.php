a10f922ec2bc3c0735600d9cf02f7b85239da408
petrero <petrero@gmail.com>
2024-11-14 09:36:22 +0200

    12.4 Finally, we'll create our last difficulty state. Add a new PHP class called HardState, and we’ll repeat the process again: implement the interface, generate the 2 empty methods, then copy the guts we need for victory() and paste it in. Update $this->enemyLevelBonus to the local $difficultyContext variable. All stuff we did before.
     For defeat(), grab its code from GameDifficultyContext, paste, and... do that variable rename one last time. And at the bottom, don’t forget to change the difficultyState to new MediumState()!

---
+++ src/DifficultyState/HardState.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DifficultyState;
+
+use App\Character\Character;
+use App\Dice;
+use App\FightResult;
+use App\GameApplication;
+use App\GameDifficultyContext;
+
+class HardState implements DifficultyStateInterface {
+	public function victory(GameDifficultyContext $difficultyContext, Character $player, FightResult $fightResult) {
+		switch (Dice::roll(20)) {
+			case 1:
+				$difficultyContext->enemyLevelBonus = $player->getLevel() + 5;
+				break;
+			case 20:
+				$player->setXpBonus(100);
+				break;
+			default:
+				// restore bonus settings
+				$difficultyContext->enemyLevelBonus = $player->getLevel() + 1;
+				$player->setXpBonus(50);
+				break;
+		}
+	}
+
+	public function defeat(GameDifficultyContext $difficultyContext, Character $player, FightResult $fightResult) {
+		if ($fightResult->getLoseStreak() >= 2) {
+			$difficultyContext->enemyHealthBonus = 5;
+			$difficultyContext->enemyAttackBonus = 5;
+			$player->setXpBonus(25);
+			$difficultyContext->difficultyState = new MediumState();
+
+			GameApplication::$printer->info('Game difficulty level decreased to Medium!');
+		}
+	}
+
+}
