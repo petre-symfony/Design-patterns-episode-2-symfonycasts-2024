0eaa15932b116674c5e84f9285216b2d7e6b1e27
petrero <petrero@gmail.com>
2024-11-14 09:50:47 +0200

    12.6 This is working nicely, but you may not be the biggest fan of how we instantiated those state objects. What if they had some dependencies? Or what if they were expensive to create? Great questions! If the states are simple to create, do what we did, because it will keep your code simple. If they have dependencies and are expensive to create, leverage the AutowireLocator attribute to inject them lazily and reuse the same instance. If you need a fresh state object every time, use a factory to create them, and inject it into the GameDifficultyContext. We'll talk more about factories soon.
     Alright! Our new setup makes it super easy to add new levels to the game. If we wanted to add a "hardest" level, we would just add a new state class and make a tiny change to the HardState so it moves us up to the "hardest" level in the victory() method. It's that simple!
    
     Next: Let's take a look at the State pattern in the real world!

--- src/GameApplication.php
+++ src/GameApplication.php
@@ -34,7 +34,6 @@ class GameApplication {
 
 	public function play(Character $player, Character $ai, FightResultSet $fightResultSet): void {
 		while (true) {
-			$player->setHealth(100);
 			$fightResultSet->addRound();
 			GameApplication::$printer->writeln([
 				'------------------------------',
